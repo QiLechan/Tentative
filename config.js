a9df93216bf31fbde0560d3534c8757380db4f0c
七楽 <88995341+QiLechan@users.noreply.github.com>
2022-12-16 10:44:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '状态监控',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1842327-a693877611cd67f1d5ac1bba'
   ],
 
   // 日志天数
@@ -19,7 +16,7 @@ window.Config = {
   ShowLink: true,
 
   // 导航栏菜单
-  Navi: [
+  /*Navi: [
     {
       text: 'Homepage',
       url: 'https://status.org.cn/'
@@ -33,4 +30,4 @@ window.Config = {
       url: 'https://abo.xyz/'
     },
   ],
-};
+};*/
